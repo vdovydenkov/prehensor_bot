772a125a7ae9f8c3596f642cd6c67b31b51f2577
VDovydenkov <dov@mlbox.ru>
2025-05-04 13:19:33 +0300

    Добавлен хендлер для лога ошибок; Добавлены константы с именами отладочного лог-файла и лог-файла с ошибками; функция переменована из add_file_handler в ..._handlers.

--- bot/services/reporter.py
+++ bot/services/reporter.py
@@ -4,18 +4,21 @@ from logging.handlers import RotatingFileHandler
 import os
 from pathlib import Path
 
+DEBUG_LOG = 'prehensor_bot_debug.log'
+ERRORS_LOG = 'prehensor_bot_errors.log'
+
 def set_console_logger(name: str = 'prehensor') -> logging.Logger:
     """
-    Настройка консольного логгирования на уровне DEBUG.
+    Настройка консольного логгирования на уровне INFO.
     Вызывается до загрузки .env и логирования в файл.
     """
     logger = logging.getLogger(name)
     if any(isinstance(h, logging.StreamHandler) for h in logger.handlers):
         return logger
 
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     console = logging.StreamHandler()
-    console.setLevel(logging.DEBUG)
+    console.setLevel(logging.INFO)
     fmt = logging.Formatter(
         '%(asctime)s %(message)s',
         datefmt='%H:%M:%S'
@@ -24,29 +27,44 @@ def set_console_logger(name: str = 'prehensor') -> logging.Logger:
     logger.addHandler(console)
     return logger
 
-def add_file_handler(logger: logging.Logger, log_dir: str) -> None:
+def add_file_handlers(logger: logging.Logger, log_dir: str) -> None:
     """
-    После успешной загрузки .env — добавляем файловый хендлер.
+    После успешной загрузки .env — добавляем файловые хендлеры:
+    prehensor_bot_debug.log - для отладочных сообщений;
+    prehensor_bot_error.log - для ошибок.
     """
     # не добавляем, если уже есть RotatingFileHandler с таким файлом
     for h in logger.handlers:
-        if isinstance(h, RotatingFileHandler) and h.baseFilename.endswith('app.log'):
+        if isinstance(h, RotatingFileHandler) \
+           and (h.baseFilename.endswith(DEBUG_LOG) \
+           or h.baseFilename.endswith(ERRORS_LOG)):
             return
     # создаём папку, если надо
     path = Path(log_dir)
     path.mkdir(parents=True, exist_ok=True)
 
-    file_path = path / 'prehensor.log'
-    file_rot = RotatingFileHandler(
-        filename=str(file_path),
-        maxBytes=1_000_000,
-        backupCount=5
-    )
-    file_rot.setLevel(logging.DEBUG)
     fmt = logging.Formatter(
         '%(asctime)s %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
+
+    # Сначала отладочный лог
+    file_path = path / DEBUG_LOG
+    file_rot = RotatingFileHandler(
+        filename=str(file_path),
+        maxBytes=1_000_000,
+        backupCount=2
+    )
+    file_rot.setLevel(logging.DEBUG)
     file_rot.setFormatter(fmt)
     logger.addHandler(file_rot)
-
+    # Теперь лог с ошибками
+    file_path = path / ERRORS_LOG
+    file_rot = RotatingFileHandler(
+        filename=str(file_path),
+        maxBytes=1_000_000,
+        backupCount=2
+    )
+    file_rot.setLevel(logging.ERROR)
+    file_rot.setFormatter(fmt)
+    logger.addHandler(file_rot)
