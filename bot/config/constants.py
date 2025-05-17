edb64c8d986f85b853c541bf2fc1cefe74b87da4
VDovydenkov <dov@mlbox.ru>
2025-05-17 23:38:25 +0300

    Константы вынесены в bot/config/constants

---
+++ bot/config/constants.py
@@ -0,0 +1,10 @@
+# Константы логгера
+DEBUG_LOG       = 'prehensor_bot_debug.log'
+ERRORS_LOG      = 'prehensor_bot_errors.log'
+LOG_MSG_FMT     = '%(asctime)s %(message)s'
+LOG_DATEFMT     = '%H:%M:%S'
+LOG_MAXSIZE     = 1_000_000
+LOG_BACKUPCOUNT = 2
+
+# Имена файлов
+YAML_SETTINGS = 'settings.yaml'
