e81437f0d6663656995c1c5410370bf8465b69b8
VDovydenkov <dov@mlbox.ru>
2025-05-22 23:44:58 +0300

    Добавлены обработки исключений TelegramError; добавлены соответствующие сообщения об ошибках.

--- bot/__main__.py
+++ bot/__main__.py
@@ -11,6 +11,12 @@ add_console_handler(logger)
 logger.info('Консольный логгер инициализирован.')
 
 from telegram.ext import ApplicationBuilder
+from telegram.error import (
+    Conflict,
+    NetworkError,
+    InvalidToken,
+    TelegramError,
+)
 
 from bot.config.configurator import Cfg
 from bot.handlers import register_handlers
@@ -33,8 +39,18 @@ def main():
     register_handlers(app)
 
     logger.info('Запускаем бота.')
-    app.run_polling()
-    logger.info('Завершаем без ошибок.')
+    try:
+        app.run_polling()
+    except Conflict:
+        logger.error(cfg.err.bot_conflict)
+    except NetworkError:
+        logger.error(cfg.err.network_error)
+    except InvalidToken:
+        logger.error(cfg.err.invalid_token)
+    except TelegramError as err:
+        logger.error(cfg.err.other_telegram_error, exc_info=True)
+
+    logger.info('Завершаемся.')
 
 if __name__ == '__main__':
     main()
